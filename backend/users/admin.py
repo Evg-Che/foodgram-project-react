68bafd4c36308bc5732e329d0aa4d7bd115afd4b
Evgeniya <evgesha.che@yandex.ru>
2023-08-31 23:33:41 +0200

    Приложение `users`
    - Удалены лишние файлы
    - Реализованы модели
    - Реализовано админка

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Subscription, User
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Класс управления отображением данных пользователя Foodgram."""
+
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name'
+    )
+    search_fields = (
+        'email',
+        'username'
+    )
+    list_filter = (
+        'first_name',
+        'last_name'
+    )
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    """Класс управления отображением данных подписчиков Foodgram."""
+
+    list_display = (
+        'user', 'author'
+    )
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Subscription(models.Model):
+    """Класс управления данными подписок пользователей на авторов."""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_following'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('author')),
+                name='no_self_subscription'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.author}'
