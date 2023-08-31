ccd5dbe7ee14694d4e109008b7bb8ea6410c44e4
Evgeniya <evgesha.che@yandex.ru>
2023-08-31 22:31:29 +0200

    Импорт данных
    - Улучшено загрузка данных в БД

--- backend/recipes/management/commands/load_models.py
+++ backend/recipes/management/commands/load_models.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from django.core.management.base import BaseCommand
 
@@ -6,9 +7,23 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    _FILE_PATH = 'recipes/data/ingredients.json'
+    _FILE_PATH = os.path.join('recipes/data', 'ingredients.json')
 
     def handle(self, *args, **options):
+        if Ingredient.objects.exists():
+            print('Данные уже загружены в базу данных Foodgram.')
+            return
+
+        try:
+            self.load_models()
+        except FileNotFoundError:
+            print(f'Файл {self.FILE_PATH} не найден')
+        except Exception as err:
+            print(f'Не удалось загрузить данные: {err}')
+        else:
+            print('Данные успешно добавлены в базу данных Foodgram.')
+
+    def load_models(self):
         with open(self._FILE_PATH, 'r', encoding='UTF-8') as file:
             data = json.load(file)
             Ingredient.objects.bulk_create(
